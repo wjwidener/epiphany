d02d7f28c354c7f5dd15256d018695be3127f3cd
przemyslavic <43173646+przemyslavic@users.noreply.github.com>
2020-05-05 14:04:25 +0200

    Allow traffic from all vms in the cluster; minor test fixes (#1235)

--- core/src/epicli/tests/serverspec-cli/spec/nfs/nfs_spec.rb
+++ core/src/epicli/tests/serverspec-cli/spec/nfs/nfs_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 nfs_defs = readDataYaml("configuration/nfs")["specification"]["nfs_defs"]
+nfs_default_port = 2049
+rpcbind_default_port = 111
 
 if os[:family] == 'redhat'
   describe 'Checking if NFS service is running' do
@@ -22,6 +24,16 @@ elsif os[:family] == 'ubuntu'
   end
 end
 
+describe 'Checking if the ports are open' do
+  let(:disable_sudo) { false }
+  describe port(nfs_default_port) do
+    it { should be_listening }
+  end
+  describe port(rpcbind_default_port) do
+    it { should be_listening }
+  end
+end  
+
 describe 'Checking NFS export file' do
   describe file('/etc/exports') do
     it { should exist }
@@ -30,8 +42,9 @@ describe 'Checking NFS export file' do
 end
 
 describe 'Checking available NFS mounts' do
+  let(:disable_sudo) { false }
   nfs_defs.select {|i|
-    describe command("/usr/sbin/showmount -e localhost | grep #{i['nfs_path'].chomp('/')}") do
+    describe command("showmount -e localhost | grep #{i['nfs_path'].chomp('/')}") do
       its(:stdout) { should match /^#{i["nfs_path"].chomp("/")}*/}
       its(:exit_status) { should eq 0 }
     end}
